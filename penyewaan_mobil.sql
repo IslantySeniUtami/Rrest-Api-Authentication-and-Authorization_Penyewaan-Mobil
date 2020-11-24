e32dd334bac426496726b69f8669220c0ec2b507
Islanty Seni Utami <46374176+IslantySeniUtami@users.noreply.github.com>
2020-11-24 21:09:06 +0700

    Add files via upload

---
+++ penyewaan_mobil.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Nov 2020 pada 15.05
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penyewaan_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` varchar(100) NOT NULL,
+  `nama_karyawan` varchar(300) NOT NULL,
+  `alamat_karyawan` varchar(300) NOT NULL,
+  `kontak` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `alamat_karyawan`, `kontak`, `username`, `password`) VALUES
+('1', 'Nina', 'Malang', '082156478932', 'nina12', '827ccb0eea8a706c4c34a16891f84e7b'),
+('2', 'Robi', 'Surabaya', '082176824154', 'robi66', '827ccb0eea8a706c4c34a16891f84e7b'),
+('3', 'Roni', 'Pekalongan', '082198127634', 'roni32', '827ccb0eea8a706c4c34a16891f84e7b'),
+('4', 'Tomi', 'Malang', '082165894321', 'tomii8', '827ccb0eea8a706c4c34a16891f84e7b'),
+('5', 'Iin', 'Surabaya', '082199884521', 'iin123', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` varchar(100) NOT NULL,
+  `nomor_mobil` varchar(100) NOT NULL,
+  `merk` varchar(200) NOT NULL,
+  `jenis` varchar(200) NOT NULL,
+  `warna` varchar(200) NOT NULL,
+  `tahun_pembuatan` varchar(20) NOT NULL,
+  `biaya_sewa_per_hari` double NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `nomor_mobil`, `merk`, `jenis`, `warna`, `tahun_pembuatan`, `biaya_sewa_per_hari`, `image`) VALUES
+('1', 'N 5678 SN', 'Honda', 'Jazz', 'Biru', '1955', 5, 'jazz.jpg'),
+('2', 'N 1265 TB', 'Honda', 'Ayla', 'Ungu', '1975', 4.5, 'ayla.jpg'),
+('3', 'N 8934 KH', 'Honda', 'Brio', 'Merah', '1970', 5.5, 'brio.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` varchar(100) NOT NULL,
+  `nama_pelanggan` varchar(300) NOT NULL,
+  `alamat_pelanggan` varchar(300) NOT NULL,
+  `kontak` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat_pelanggan`, `kontak`) VALUES
+('1', 'sulaiman', 'malang', '034156743216'),
+('2', 'hari', 'surabaya', '034191836454'),
+('3', 'kirman', 'situbondo', '034134215678'),
+('4', 'irman', 'malang', '034198765432');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sewa`
+--
+
+CREATE TABLE `sewa` (
+  `id_sewa` varchar(200) NOT NULL,
+  `id_mobil` varchar(100) NOT NULL,
+  `id_karyawan` varchar(100) NOT NULL,
+  `id_pelanggan` varchar(100) NOT NULL,
+  `tgl_sewa` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `total_bayar` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `sewa`
+--
+
+INSERT INTO `sewa` (`id_sewa`, `id_mobil`, `id_karyawan`, `id_pelanggan`, `tgl_sewa`, `tgl_kembali`, `total_bayar`) VALUES
+('3', '3', '2', '3', '2020-11-23', '2020-11-25', 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `sewa`
+--
+ALTER TABLE `sewa`
+  ADD PRIMARY KEY (`id_sewa`),
+  ADD KEY `id_mobil` (`id_mobil`,`id_karyawan`,`id_pelanggan`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `sewa`
+--
+ALTER TABLE `sewa`
+  ADD CONSTRAINT `sewa_ibfk_1` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`),
+  ADD CONSTRAINT `sewa_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`),
+  ADD CONSTRAINT `sewa_ibfk_3` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
